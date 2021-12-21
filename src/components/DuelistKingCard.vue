c8a12c7783f2dfd0df67501c2753b76ffd8222c4
wpj3 <willie.putra.johnson@gmail.com>
2021-12-21 09:20:52 +0800

    compressed assets

--- src/components/DuelistKingCard.vue
+++ src/components/DuelistKingCard.vue
@@ -2,11 +2,16 @@
 import { computed } from "vue";
 import cards from "../data/cards.json";
 
+interface ImageUrl {
+  jpg: string;
+  webp: string;
+}
+
 interface Card {
   id: string;
   name: string;
   description: string;
-  imageUrl: string;
+  image: ImageUrl;
 }
 
 const props = defineProps<{ id: string; count: number }>();
@@ -17,12 +22,16 @@ const count = computed(() => Intl.NumberFormat().format(props.count));
 
 <template>
   <div class="relative w-full min-h-80 aspect-card overflow-hidden">
-    <img
-      class="w-full h-full object-center object-cover"
-      :src="card.imageUrl"
-      :alt="card.name"
-      :title="card.name"
-    />
+    <picture>
+      <source type="image/webp" :srcset="card.image.webp" />
+      <source type="image/jpeg" :srcset="card.image.jpg" />
+      <img
+        class="w-full h-full object-center object-cover"
+        :src="card.image.jpg"
+        :alt="card.name"
+        :title="card.name"
+      />
+    </picture>
     <div
       class="
         absolute
